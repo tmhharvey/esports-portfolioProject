0561649bbbc8f0093cd263fb76b92ae9bac7fe99
Taylor Harvey <larothion@aol.com>
2018-12-05 16:57:32 -0700

    heroku issues

--- app.js
+++ app.js
@@ -4,6 +4,4 @@ const app = express();
 
 app.use(express.static(__dirname));
 
-app.listen(3000, () => {
-  console.log("app is listening for request on port 3000");
-});
+app.listen(process.env.PORT || 3000);
